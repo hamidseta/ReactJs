8d3e31e9c54965eb694cf995bf2f2180bcbd4a61
hamidsetacni <hamid.seta@castingnetworks.in>
2021-08-22 18:21:02 +0530

    User Json server and Use data in application

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,12 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home =() =>{
-    const [blogs,setBlogs] = useState([
-        {title:'My new website',body:'This is new website created in react JS',author:'hamid',id:1},
-        {title:'Myntra Site',body:'Myntra is shopping site',author:'bansal',id:2},
-        {title:'Amazon site',body:'Amazon is a also a leading shopping site',author:'Jac',id:3},
-        {title:'Filpcart site',body:'This is new website created in react JS',author:'Bandal bhai',id:4}        
-    ])
+    const [blogs,setBlogs] = useState(null)
     
     const handleDelete =(id)=>{
         const newBlogs = blogs.filter(blog=>blog.id !== id)
@@ -16,12 +11,18 @@ const Home =() =>{
 
     useEffect(()=>{
         console.log('use effect ran')
-    })
+        fetch('http://localhost:8000/Blogs')
+        .then(res=>{
+            return res.json();
+        }).then((data)=>{
+            setBlogs(data)
+        })
+    },[]);
     
     return (
         <div className="home">
             <h2>Home</h2>
-            <BlogList blogs={blogs} title={'All the blogs!'} handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title={'All the blogs!'} handleDelete={handleDelete}/>}
         </div>
     )
 }
